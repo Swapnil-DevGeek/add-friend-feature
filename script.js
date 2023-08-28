9780ad556b570a56ffbdce4c8ea8e55df32d55dc
Swapnil-DevGeek <133921087+Swapnil-DevGeek@users.noreply.github.com>
2023-08-28 19:30:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var isstatus = document.querySelector("h5")
+
+var btn = document.querySelector("#add")
+var flag = 0
+
+btn.addEventListener("click",function(){
+    if(flag==0){
+        isstatus.innerHTML = "Friends"
+        isstatus.style.color = "green"
+        btn.innerHTML = "Remove Friend"
+        flag =1
+    }else{
+        isstatus.innerHTML = "Stranger"
+        isstatus.style.color = "red"
+        btn.innerHTML = "Add Friend"
+        flag =0
+    }
+
+})
+
